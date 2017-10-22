885f97cc1cecb1ccb69a0cb4ead007297dac4941
danny perez <perez6736@gmail.com>
2017-10-22 18:26:40 -0400

    added spotify keys and npm

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '907678696441606145-2PeSMLWrE3zzjsgmgwJdDkifi2sukz5',
   access_token_secret: 'vh5xSCl7LfNH9djnNJf0L7vPYKOHI2kj5fEur6poQ7eIs',
 }
+
+exports.spotifyKeys = {
+  clientid: "22e69318a05344d687c2e564eab781cc",
+  client_secret: "5ca5db7d53db40dcaab32d9278a06890"
+}
--- liri.js
+++ liri.js
@@ -1,12 +1,22 @@
 var keys = require("./keys.js");
 var inquirer = require('inquirer');
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 // twitter keys 
 var consumer_key = keys.twitterKeys.consumer_key;
 var consumer_secret = keys.twitterKeys.consumer_secret;
 var access_token_key = keys.twitterKeys.access_token_key;
 var access_token_secret = keys.twitterKeys.access_token_secret;
+
+//spotify keys
+var clientID = keys.spotifyKeys.clientid;
+var clientSecret = keys.spotifyKeys.client_secret;
+
+var spotify = new Spotify({
+    id: clientID,
+    secret: clientSecret
+  });
 
 var client = new Twitter({
     consumer_key: consumer_key,
@@ -20,10 +30,9 @@ var nodeArgs = process.argv;
 // the command will be the firsdt thing the user types in the command console. 
 var command = nodeArgs[2];
 
-
-//twitter part of hw
-if(command === "my-tweets"){
-    var params = {screen_name: 'dpereztwitbot'};
+//function that gets tweets 
+function getTweets(username){
+    var params = {screen_name: username};
     client.get('statuses/user_timeline', params, function(error, tweets, response){
         if(error){
             console.log("Something has gone wrong.");
@@ -31,5 +40,30 @@ if(command === "my-tweets"){
         for(i=0; i<20; i++){
             console.log(tweets[i].text);
         }
-    })
+    });
+}
+
+function getSpotifyInformation(){
+
+}
+
+function getMovieInfo(){
+
+}
+
+//twitter part of hw
+if(command === "my-tweets"){
+    getTweets('dpereztwitbot');
+}
+
+else if(command === "spotify-this-song"){
+    getSpotifyInformation();
+}
+
+else if(command === "movie-this"){
+    getMovieInfo();
+}
+
+else if(command === "do-what-it-says"){
+    // do whatever command is in the text 
 }
